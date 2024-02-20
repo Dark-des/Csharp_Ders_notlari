324d86e3d3bd8d5fa6e46d54a85102ec062ff0e7
huday <huday@LAPTOP-V5H9L7IL>
2024-02-20 16:23:46 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+  {
+     static void Main(string[] args)
+  {
+            // tür dönüşümleri //  
+            int a = 0;   
+            int b = new int();
+            string c = a.ToString() + b.ToString();
+            string s = "";
+            string e = "merhaba \n";
+            string t = "feyza";
+            string p = e+ t;
+            System.Console.WriteLine(c + "\n" + p);
+
+            byte veri = 5;
+            byte veri2 = 9;
+            int veri3 = veri+veri2;
+
+            int veri4 = a;
+            int veri5 = veri4 + 8;
+            int veri6 = a*veri4;
+            bool veri7 = false;
+            s = a.ToString();
+            s = s + "8";
+
+            Console.ReadLine();
+           
+            
+        }
+    }
+}
+
